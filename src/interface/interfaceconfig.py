dee8aed86beb4c845a902791baf978343c230d37
Darin <darinjacosta@gmail.com>
2016-08-27 11:02:51 -0500

    update interface config

--- src/interface/interfaceconfig.py
+++ src/interface/interfaceconfig.py
@@ -1,24 +1,19 @@
 import libtcodpy as libtcod
 import gameconfig
 
-def initialize_interface():
-    # here we go!
-    libtcod.console_set_custom_font('data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-    libtcod.console_init_root(gameconfig.SCREEN_WIDTH, gameconfig.SCREEN_HEIGHT, 'Office_Hack', False)
-    con = libtcod.console_new(gameconfig.MAP_WIDTH, gameconfig.MAP_HEIGHT) # creates con
-    panel = libtcod.console_new(gameconfig.SCREEN_WIDTH, gameconfig.PANEL_HEIGHT) #creates panel
-    game_msgs = []
+# public vars
+con = libtcod.console_new(gameconfig.MAP_WIDTH, gameconfig.MAP_HEIGHT)
+panel = libtcod.console_new(gameconfig.SCREEN_WIDTH, gameconfig.PANEL_HEIGHT)
+game_msgs = []
+key = libtcod.Key()
+mouse = libtcod.Mouse()
 
-    libtcod.sys_set_fps(gameconfig.LIMIT_FPS) #FPS
-
-    libtcod.console_set_default_foreground(0, libtcod.light_yellow)
-    libtcod.console_print_ex(0, gameconfig.SCREEN_WIDTH/2, gameconfig.SCREEN_HEIGHT/2-4, libtcod.BKGND_NONE, libtcod.CENTER,
-        gameconfig.GAME_TITLE)
-    libtcod.console_print_ex(0, gameconfig.SCREEN_WIDTH/2, gameconfig.SCREEN_HEIGHT/2-3, libtcod.BKGND_NONE, libtcod.CENTER,
-        gameconfig.GAME_AUTHOR)
-    return con, panel, game_msgs
-
-def initialize_controls():
-    key = libtcod.Key()
-    mouse = libtcod.Mouse()
-    return key, mouse
+# set features
+libtcod.console_set_custom_font('data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(gameconfig.SCREEN_WIDTH, gameconfig.SCREEN_HEIGHT, 'Office_Hack', False)
+libtcod.sys_set_fps(gameconfig.LIMIT_FPS) #FPS
+libtcod.console_set_default_foreground(0, libtcod.light_yellow)
+libtcod.console_print_ex(0, gameconfig.SCREEN_WIDTH/2, gameconfig.SCREEN_HEIGHT/2-4, libtcod.BKGND_NONE, libtcod.CENTER,
+    gameconfig.GAME_TITLE)
+libtcod.console_print_ex(0, gameconfig.SCREEN_WIDTH/2, gameconfig.SCREEN_HEIGHT/2-3, libtcod.BKGND_NONE, libtcod.CENTER,
+    gameconfig.GAME_AUTHOR)
--- src/officehack.py
+++ src/officehack.py
@@ -1,25 +1,23 @@
 import libtcodpy as libtcod
 import gameconfig
 from interface.menus import main_menu
-from interface.interfaceconfig import initialize_interface
+from interface import interfaceconfig
 from interface.helpers import message, message_box
 from game.states import new_game, play_game, save_game
-
-con, panel, game_msgs = initialize_interface() #setup screen here
 
 choice = main_menu()
 
 while not libtcod.console_is_window_closed():
     if choice == 0:
-        key, mouse, inventory, player, objects, level_map, stairs, fov_map, fov_recompute = new_game()
+        inventory, player, objects, level_map, stairs, fov_map, fov_recompute = new_game()
         message(gameconfig.WELCOME_MESSAGE, libtcod.red)
-        play_game(player, objects, level_map, fov_map, key, mouse, con, panel)
+        play_game(player, objects, level_map, fov_map)
     if choice == 1:
         try:
             load_game()
         except:
             message_box('\n No saved gamedata to load.\n', 24)
             continue
-        play_game(player, objects, key, mouse)
+        play_game(player, objects)
     elif choice == 2:
         break
