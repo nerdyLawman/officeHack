10372b7200be8f00e9467a486bc1b206fa2ee8ae
Darin <darinjacosta@gmail.com>
2016-09-09 16:33:52 -0500

    only import mixer

--- src/sound/SoundPlayer.py
+++ src/sound/SoundPlayer.py
@@ -4,9 +4,9 @@ from random import randint
 class SoundPlayer:
     def __init__(self, sound_arg, **kwargs):
         if gameconfig.SOUND:
-            import pygame
-            self.pygame = pygame
-            self.pygame.mixer.init()
+            from pygame import mixer
+            self.mixer = mixer
+            self.mixer.init()
             init_loop = kwargs.get('loop', False)
             if type(sound_arg) is dict:
                 sound = sound_arg['name'] + str(randint(1, sound_arg['number']))
@@ -20,7 +20,7 @@ class SoundPlayer:
                 self.loop = -1
                 gameconfig.CURRENT_TRACK = self
             self.sound_path = 'data/sound/' + sound + '.wav'
-            self.sound = self.pygame.mixer.Sound(self.sound_path)
+            self.sound = self.mixer.Sound(self.sound_path)
             self.sound.set_volume(self.volume)
         else:
             # if sound is false use empty object as current track
@@ -43,6 +43,6 @@ class SoundPlayer:
         if hasattr(self, 'sound'):
             self.sound.fadeout(2000)
             self.sound_path = 'data/sound/' + new_track + '.wav'
-            self.sound = self.pygame.mixer.Sound(self.sound_path)
+            self.sound = self.mixer.Sound(self.sound_path)
             self.sound.set_volume(self.volume)
             self.play()
